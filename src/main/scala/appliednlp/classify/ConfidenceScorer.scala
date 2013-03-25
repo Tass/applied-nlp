3bba4d014f09b4e1b0598f1387e2a6533b64d567
Simon Hafner <hafnersimon@gmail.com>
2013-03-25 04:55:45 -0500

    confidence scorer

--- src/main/scala/appliednlp/classify/ConfidenceScorer.scala
+++ src/main/scala/appliednlp/classify/ConfidenceScorer.scala
@@ -8,8 +8,48 @@ package appliednlp.classify
  */
 object ConfidenceScorer {
 
+  import org.rogach.scallop._
+
   def main(args: Array[String]) {
+    val opts = ConfidenceScorerOpts(args)
 
-  }  
+    val lines = io.Source.fromFile(opts.goldFile()).getLines.map(_.split(" ").last)
+      .zip(io.Source.fromFile(opts.predictFile()).getLines
+      .map({line => val splitted = line.split(" ")
+            // Parse the predicted data
+           (splitted(0), splitted(1).toDouble)
+          }))
+       .map({case (gold, (result, confidence)) =>
+             // Check if correct
+             (confidence, result(0) == gold(0))
+           })
+    // returns (confidence, correct)
+      .toList.sortBy(_._1)
 
+    val size = lines.size
+    val result = Seq(lines.take(size/3), lines.drop(size/3).take(size/3), lines.drop(size/3*2)).map(
+      lines => lines.count(_._2).toDouble/lines.size
+      )
+    val top = result(2)
+    val middle = result(1)
+    val lower = result(0)
+    println(s"High confidence accuracy: $top")
+    println(s"Mid confidence accuracy: $middle")
+    println(s"Low confidence accuracy: $lower")
+  }
+
+}
+
+
+object ConfidenceScorerOpts {
+
+  import org.rogach.scallop._
+  
+  def apply(args: Array[String]) = new ScallopConf(args) {
+    banner("""
+For usage see below:
+	     """)
+    val goldFile = trailArg[String]("gold", descr = "File containing the gold standard")
+    val predictFile = trailArg[String]("predict", descr = "File containing the predictions")
+  }
 }
